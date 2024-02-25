78cd408f2db9a2c3a31cceabf4a01c1ace3d490a
ol416 <ol416@outlook.com>
2024-02-25 14:29:09 +0800

    删除开头和结尾的参考下

---
+++ remove_guides_fl.jsx
@@ -0,0 +1,15 @@
+﻿#target photoshop
+<javascriptresource>
+
+<name>删除开头和结尾的参考线</name>
+
+<category>custom</category>
+
+</javascriptresource>
+try{
+    var ad = activeDocument;
+    var guides_len = ad.guides.length;
+    ad.guides[guides_len-1].remove();
+    ad.guides[0].remove();
+ }catch (e) {       
+}
