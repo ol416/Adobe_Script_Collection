8992a42001bf15c03c598a52d70869ecba0489a8
ol416 <ol416@outlook.com>
2024-02-25 12:01:16 +0800

    在photoshop中批量置入智能链接对象

---
+++ import_linked_smart_object.jsx
@@ -0,0 +1,70 @@
+#target photoshop
+<javascriptresource>
+
+<name>批量置入链接智能对象</name>
+
+<category>custom</category>
+
+</javascriptresource>
+
+// 获取当前打开的文档
+var doc = app.activeDocument;
+
+// 选择要插入的外部文件
+var files = File.openDialog("选择要插入的文件", "*.psd;*.jpg;*.png", true);
+
+function showError(err) {
+	if (confirm('An unknown error has occurred.\n' +
+		'Would you like to see more information?', true, 'Unknown Error')) {
+			alert(err + ': on line ' + err.line, 'Script Error', true);
+	}
+}
+
+function placeEvent(ID, null2, linked, horizontal, vertical) {
+	var c2t = function (s) {
+		return app.charIDToTypeID(s);
+	};
+
+	var s2t = function (s) {
+		return app.stringIDToTypeID(s);
+	};
+
+	var descriptor = new ActionDescriptor();
+	var descriptor2 = new ActionDescriptor();
+
+	descriptor.putInteger( s2t( "ID" ), ID );
+	descriptor.putPath( c2t( "null" ), null2 );
+	descriptor.putBoolean( s2t( "linked" ), linked );
+	descriptor.putEnumerated( s2t( "freeTransformCenterState" ), s2t( "quadCenterState" ), s2t( "QCSAverage" ));
+	descriptor2.putUnitDouble( s2t( "horizontal" ), s2t( "distanceUnit" ), horizontal );
+	descriptor2.putUnitDouble( s2t( "vertical" ), s2t( "distanceUnit" ), vertical );
+	descriptor.putObject( s2t( "offset" ), s2t( "offset" ), descriptor2 );
+	executeAction( s2t( "placeEvent" ), descriptor, DialogModes.NO );
+}
+
+// 遍历选定的文件并插入为链接智能对象图层
+try {
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        
+        // 插入链接智能对象图层
+        placeEvent(2, file, true, 0, 0);
+
+        // var smartObjectLayer = doc.artLayers.add();
+        // smartObjectLayer.name = "Linked Smart Object " + (i + 1);
+        // smartObjectLayer.kind = LayerKind.SMARTOBJECT;
+        
+        // // 将外部文件链接到智能对象
+        // var smartObject = smartObjectLayer.smartObject;
+        // smartObject.link(file);
+    }
+}
+catch(e) {
+    // don't report error on user cancel
+    // if (e.number != 8007) {
+    //     showError(e);
+    // }
+}
+
+// 提示完成
+// alert("批量置入链接智能对象完成！");
