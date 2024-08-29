778c43e93cb39b9803a23b81121bfcb04e6ece27
ol416 <ol4160@outlook.com>
2024-08-29 12:18:04 +0800

    feat: 修改图片保存功能，增加压缩选项

--- save_png_current_path.jsx
+++ save_png_current_path.jsx
@@ -9,9 +9,15 @@
 var prefix = "";
 // var docName = activeDocument.name.replace(/\.[^\.]+$/, '');
 var docName = "30" //将图片保存为30.png
-var saveFile = new File(activeDocument.path + "/" + prefix + docName);
+var savePath = activeDocument.path; // 缓存路径
 
-pngSaveOptions = new PNGSaveOptions();
-pngSaveOptions.quality = 100;
+function saveAsPNG(fileName, quality,compression) {
+  var saveFile = new File(savePath + "/" + prefix + fileName + ".png");
+  var pngSaveOptions = new PNGSaveOptions();
+  pngSaveOptions.quality = quality;
+  pngSaveOptions.compression = compression; // 使用 ZIP 压缩
+  activeDocument.saveAs(saveFile, pngSaveOptions, true, Extension.LOWERCASE);
+}
 
-activeDocument.saveAs(saveFile, pngSaveOptions, true, Extension.LOWERCASE);
+// 示例用法：
+saveAsPNG(docName, 100,6); // 保存为质量为 100 的 PNG
